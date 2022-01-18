104a7b52c28ab7a07d60485ecd42382b1496b298
Sean Mangan <spmangan2000@gmail.com>
2022-01-17 20:27:12 -0600

    fixed cors policy

--- handler.js
+++ handler.js
@@ -3,6 +3,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const app = express();
+var cors = require('cors')
+
+var corsOptions = {
+  origin: 'http://hattavick.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors())
 
 // init session
 app.use(session({
@@ -23,11 +31,24 @@ app.use(bodyParser.json());
 const userRoutes = require('./routes/users');
 app.use("/api/login", userRoutes);
 
-
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from home",
   });
+});
+
+
+app.get("/logout", (req, res, next) => {
+  if(req.session.user){
+    req.session.destroy();
+    return res.status(200).json({
+      message: "Successfully logged out",
+    });
+  }else{
+    return res.status(404).json({
+      error: "Not yet logged in",
+    });
+  }
 });
 
 app.use((req, res, next) => {
