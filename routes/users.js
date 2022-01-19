f1100aa106924fe083c6af548775042bf4847026
Sean Mangan <spmangan2000@gmail.com>
2022-01-19 10:49:34 -0600

    added functionality to see if session is active for login

--- routes/users.js
+++ routes/users.js
@@ -6,25 +6,30 @@ const client = require('../database/database');
 router.post("/", (req, res, next) => {
   console.log("USER REQUEST", req.body)
   if(req.session.user){
-    res.status(200).json({name: req.session.user});
+    console.log("already logged in")
+    res.status(200).json({"name": req.session.user});
   }else{
-    client.connect(err => {
-      client.db("Hattavick").collection("user_data").findOne(req.body, (err,result) => {
-        try {
-          console.log(req.session.user)
-          if (err) throw err
-          if (result) {
-            req.session.user = result.name
-            res.status(200).json(result);
-          } else{
-            res.status(404).json({error: "Not Found"})
+    if (req.body.name){
+      client.connect(err => {
+        client.db("Hattavick").collection("user_data").findOne(req.body, (err,result) => {
+          try {
+            console.log(req.session.user)
+            if (err) throw err
+            if (result) {
+              req.session.user = result.name
+              res.status(200).json(result);
+            } else{
+              res.status(404).json({error: "Not Found"})
+            }
+          }catch(err) {
+            console.log(err)
+            res.status(500).json({error : 'Internal Server Error'});
           }
-        }catch(err) {
-          console.log(err)
-          res.status(500).json({'error' : 'Internal Server Error'});
-        }
+        });
       });
-    });
+    }else{
+      res.status(404).json({error: "Not Found"})
+    }
   }
 });
 
