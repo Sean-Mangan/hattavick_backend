7c9bfd61ecc1731d43aa3780ca7c3d68de505af5
Sean Mangan <spmangan2000@gmail.com>
2022-01-26 16:57:32 -0600

    added proper admin

--- routes/users.js
+++ routes/users.js
@@ -100,7 +100,7 @@ router.post("/characters", async (req, res, next) => {
 
 router.post("/allcharacters", async (req, res, next) => {
   try {
-      if (req.body && req.session.user === "sean"){
+      if (req.body && req.session.user === "Sean"){
         database.client.connect(err => {
             if (err) throw err;
             database.client.db("Hattavick").collection("npcs").find({}).toArray(function(err, result) {
