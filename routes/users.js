99ec6ca3e98c8347ccb79d7e578f34ac4cf1d4ec
Sean Mangan <spmangan2000@gmail.com>
2022-01-26 16:44:34 -0600

    added ability to get all characters

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const serverless = require("serverless-http");
 const express = require("express");
 const router = express.Router();
 const database = require('../database/database');
+const fs = require('fs');
 
 router.post("/login", (req, res, next) => {
   try{
@@ -53,6 +54,69 @@ router.post("/page", (req, res, next) => {
       }else{
           res.status(404).json({error: "Not Found"})
       }
+    }catch(err) {
+      console.log(err)
+      res.status(500).json({error : 'Internal Server Error'});
+    }
+});
+
+
+
+router.post("/characters", async (req, res, next) => {
+  try {
+      if (req.body){
+        database.client.connect(err => {
+            if (err) throw err;
+            database.client.db("Hattavick").collection("npcs").find({}).toArray(function(err, result) {
+              if (err) throw err;
+              if(result){
+                resp = []
+                for (let a_idx = 0; a_idx < result.length; a_idx++){
+                  let npc = result[a_idx]
+                  delete npc.all_notes
+                  delete npc.visible
+                  if (npc.visible){
+                    resp.push(npc)
+                  }else{
+                    resp.push({"name": "unknown"})
+                  }
+                }
+                console.log("Got data for Characters")
+                res.status(200).json(resp);
+              }else{
+                console.log("Could not find character data")
+                res.status(404).json({error: "Not Found"})
+              }
+            });
+          });
+        }else{
+            res.status(404).json({error: "Not Found"})
+        }
+    }catch(err) {
+      console.log(err)
+      res.status(500).json({error : 'Internal Server Error'});
+    }
+});
+
+router.post("/allcharacters", async (req, res, next) => {
+  try {
+      if (req.body && req.session.user === "admin"){
+        database.client.connect(err => {
+            if (err) throw err;
+            database.client.db("Hattavick").collection("npcs").find({}).toArray(function(err, result) {
+              if (err) throw err;
+              if(result){
+                console.log("Got data for Characters")
+                res.status(200).json(result);
+              }else{
+                console.log("Could not find character data")
+                res.status(404).json({error: "Not Found"})
+              }
+            });
+          });
+        }else{
+            res.status(404).json({error: "Not Found"})
+        }
     }catch(err) {
       console.log(err)
       res.status(500).json({error : 'Internal Server Error'});
